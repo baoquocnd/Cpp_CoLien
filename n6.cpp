c2ad99ceeecebe3af796fcab45ce4041dc6bb6c3
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-08-26 18:44:59 +0700

    Create n6.cpp

---
+++ n6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int tuoi;
+    getline(cin, name);
+    cin>>tuoi;
+    cout <<"In 15 years, age of "<<name<<" will be "<<tuoi+15;
+}
