952992a94edeebd2aeb2f73e3f2a5c1aefff37d3
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-08-28 09:51:13 +0700

    Create q9.cpp

---
+++ q9.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a,b; cin>>a>>b;
+    char c; cin>>c;
+    if (c=='+') cout <<a+b;
+    else if (c=='-') cout <<a-b;
+    else if (c=='*') cout <<a*b;
+    else if (c=='/') cout <<a/b;
+}
