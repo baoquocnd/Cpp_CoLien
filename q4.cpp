083686322c2d4fb6f1b591046149f8cd930cec69
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-08-28 09:37:23 +0700

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string a,b; 
+    getline(cin,a);
+    getline(cin,b);
+    if (a==b) cout<<"two people have the same name";
+    else cout<<"two people don't have the same name";
