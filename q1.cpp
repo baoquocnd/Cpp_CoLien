1f4beecd8e754746fc12264c8517ee92ef2ca115
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-08-28 09:31:44 +0700

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(){
     int n; cin>>n;
-    if (n%2==0<<) cout <<n<< " is an even number";
+    if (n%2==0) cout <<n<< " is an even number";
     else cout << n<<" is an odd number";
 }
