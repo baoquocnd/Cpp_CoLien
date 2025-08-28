f26eb39cca64fba329b20b80c8dc2c79fba37b2a
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-08-28 10:20:44 +0700

    Create qtv6.cpp

---
+++ qtv6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int a,b; cin>>a>>b;
+    for (int i = a; i <= b; i++){
+        if (i%3==0) cout << i <<" ";
+    }
+}
