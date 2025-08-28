792756ccd292523f3cc1dc4770c24a3361b36f21
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-08-28 09:48:05 +0700

    Create q7.cpp

---
+++ q7.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a; cin>>a;
+    if (a>9 &&a < 101) cout <<a<< " is in the range [10,100]";
+    else cout<<a<<" is not in the range [10,100] ";
+}
