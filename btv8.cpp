97cbb9c1945ba81cfad86ab0a5badde27d74113b
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-08-28 10:47:08 +0700

    Update btv8.cpp

--- btv8.cpp
+++ btv8.cpp
@@ -1,25 +1,20 @@
-#include <bits/stdc++.h>
+#include <bits.stdc++.h>
 using namespace std;
 
-int prime(int x) {
-    if (x < 2) return false;
-    for (int i = 2; i * i <= x; i++) {
-        if (x % i == 0) return false;
+int prime(int n){
+    for (int i = 2; i <= sqrt(n); i++){
+        if (n%i==0) return 0;
     }
-    return true;
+    return n>1;
 }
 
-int main() {
-    int n;
-    cin >> n;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            if (prime(i)) cout << i << " ";
-            if (i != n / i && prime(n / i)) cout << n / i << " ";
-        }
+int main(){
+    int n; cin>>n;
+    set <int> res;
+    for (int i = 2; i <= sqrt(n); i++){
+        if (prime(i)) res.insert(i);
+        if (i!= n/i && prime(n/i)) res.insert(n/i);
     }
-    if (prime(n)) cout << n; 
-    cout << endl;
-
-    return 0;
+    if (prime(n)) res.insert(n);
+    for (int x : res) cout <<x<<" ";
 }
