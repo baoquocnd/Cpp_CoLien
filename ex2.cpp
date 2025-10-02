b2a9cf28ae38759e0a64f5a7bd0e1dcd8fba2d39
baoquocnd <224608417+baoquocnd@users.noreply.github.com>
2025-10-02 11:26:56 +0700

    Update ex2.cpp

--- ex2.cpp
+++ ex2.cpp
@@ -30,7 +30,9 @@ int check(int n, int s){
 }
 
 void LietKe(int n, int s){
-    for (int i = )
+    for (int i = pow(10,n-1); i < pow(10,n); i++){
+        if (snt(i) && check(i,s)) cout << i<<" ";
+    }
 }
 
 int main(){
